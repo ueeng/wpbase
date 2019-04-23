2f6cb071f631b82578f980e7000435b531a5aafa
Stefanie Hetjens <stefanie@hetjens.com>
2019-04-23 17:20:17 +0200

    languages version 1

--- config/application.php
+++ config/application.php
@@ -53,6 +53,11 @@ Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
 Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
 /**
+ * Language directory
+ */
+Config::define('WP_LANG_DIR', $webroot_dir . Config::get('CONTENT_DIR') . '/languages');
+
+/**
  * DB settings
  */
 Config::define('DB_NAME', env('DB_NAME'));
